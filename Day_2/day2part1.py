3bb92ef5233e87e26a9559fbff1fc97fb95c582e
abedulator <abedulator@gmail.com>
2022-12-02 18:02:20 +0100

    Day 2 refactor

--- Day_2/day2part1.py
+++ Day_2/day2part1.py
@@ -3,27 +3,13 @@ class PPT:
         lista = archivo.readlines()
         other = [0,"A","B","C"]
         me = [0,"X","Y","Z"]
+        puntuaciones = [0,[0,3,6,0],[0,0,3,6],[0,6,0,3]]
         puntuacion = 0
         for i in lista:
             jugada_other = other.index(i[0])
             jugada_me = me.index(i[2])
-            if jugada_other == jugada_me:
-                puntuacion += 3 + jugada_me
-            elif jugada_other == 1: #roca
-                if jugada_me == 2: #papel
-                    puntuacion += 6 + jugada_me
-                else:
-                    puntuacion += jugada_me
-            elif jugada_other == 2: # papel
-                if jugada_me == 3: #tijera
-                   puntuacion += 6 + jugada_me
-                else:
-                    puntuacion += jugada_me
-            else: #tijera
-                if jugada_me == 1: #roca
-                   puntuacion += 6 + jugada_me
-                else:
-                    puntuacion += jugada_me  
+            puntuacion += puntuaciones[jugada_other][jugada_me] + jugada_me
+           
         return puntuacion
 
 
